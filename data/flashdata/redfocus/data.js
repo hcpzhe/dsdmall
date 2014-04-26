1e92da0a4b2a69c8ae6a52401a1e21c3c180fc79
wcd <774241885@qq.com>
2014-04-26 16:29:38 +0800

    更新图片

--- data/flashdata/redfocus/data.js
+++ data/flashdata/redfocus/data.js
@@ -1,13 +1,16 @@
-imgUrl1="data/afficheimg/20131022zjxxbq.jpg";
+imgUrl1="data/afficheimg/20140426xqqakm.jpg";
 imgtext1="";
 imgLink1=escape("http://");
-imgUrl2="data/afficheimg/20131022zzvsgx.jpg";
+imgUrl2="data/afficheimg/20140426ldwlqd.jpg";
 imgtext2="";
 imgLink2=escape("http://");
-imgUrl3="data/afficheimg/20131022cyfkbn.jpg";
+imgUrl3="data/afficheimg/20140426wmuera.jpg";
 imgtext3="";
 imgLink3=escape("http://");
+imgUrl4="data/afficheimg/20140426ycobmo.jpg";
+imgtext4="";
+imgLink4=escape("http://");
 
-var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3;
-var links=imgLink1+"|"+imgLink2+"|"+imgLink3;
-var texts=imgtext1+"|"+imgtext2+"|"+imgtext3;
+var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3+"|"+imgUrl4;
+var links=imgLink1+"|"+imgLink2+"|"+imgLink3+"|"+imgLink4;
+var texts=imgtext1+"|"+imgtext2+"|"+imgtext3+"|"+imgtext4;
